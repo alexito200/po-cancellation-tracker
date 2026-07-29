86b8c7ed39e6ef78b2951842b023638431328369
alexito200 <alexalarcon200@gmail.com>
2026-07-29 14:54:44 -0400

    changed file location for run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Edit the line below to match wherever you extracted poppler --
 REM specifically the folder that directly contains pdftotext.exe.
-set POPPLER_PATH=C:\Users\AlexA\OneDrive - Haddad Brands\Documents\Poppler\poppler-26.02.0\Library\bin
+set POPPLER_PATH=C:\Users\AlexA\Poppler\poppler-26.02.0\Library\bin
 
 cd /d "%~dp0"
 python -m streamlit run app.py
